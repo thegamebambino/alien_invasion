44c5b5f7d234b737adab56fa20caaa1b851a4a26
alex@panther <170464688+thegamebambino@users.noreply.github.com>
2025-08-07 12:50:17 -0500

    added gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,34 +1,27 @@
-import sys 
+import sys
 import pygame
-
+from settings import Settings
 class AlienInvasion:
-    """Overall class to manage game assets and behavior."""
-
     def __init__(self):
-        """Initialize the game and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (230, 230, 230)
-
+        
     def run_game(self):
-        """Start the main loop for the game."""
         while True:
-            self._check_events()
-            self._update_screen()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            # Update the screen with the new background color
 
-    def _check_events(self):
-        """Respond to keypresses and mouse events."""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-    def _update_screen(self):
-        """Update images on the screen and flip to the new screen."""
-        self.screen.fill(self.bg_color)
-        pygame.display.flip()
+            pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
-    # Make a game instance, and run the game.
     ai = AlienInvasion()
-    ai.run_game()
+    ai.run_game()                     
