fd880efae7ed88327cfac97209b71e6a9301bb87
alex@panther <170464688+thegamebambino@users.noreply.github.com>
2025-08-07 19:55:51 -0500

    "the ship moves across the screen, but doesn't yet fire bullets"

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,26 +1,58 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        # Initialize the background color
         self.bg_color = (230, 230, 230)
+       
         
     def run_game(self):
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+            """Respond to keypresses and mouse events."""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            # Update the screen with the new background color
+                elif event.type == pygame.KEYDOWN:
+                     self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                        self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+            """Respond to key presses."""
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+            elif event.key == pygame.K_ESCAPE:  
+                sys.exit()  
+            
+    def _check_keyup_events(self, event):
+            """Respond to key releases."""
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False    
+    def _update_screen(self):
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
-            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
